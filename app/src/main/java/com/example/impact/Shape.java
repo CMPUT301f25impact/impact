ea3690263aeef61def780dd590e983e11e61aa22
Jeremy Carefoot <jeremycarefoot@gmail.com>
2025-09-24 17:47:03 -0600

    New color field

--- app/src/main/java/com/example/impact/Shape.java
+++ app/src/main/java/com/example/impact/Shape.java
@@ -3,6 +3,7 @@ package com.example.impact;
 public abstract class Shape {
     private Integer x;
     private Integer y;
+    String color = "yellow";
 
     public Shape(Integer x, Integer y) {
         this.x = x;

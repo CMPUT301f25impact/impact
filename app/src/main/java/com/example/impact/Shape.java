b96880a256d07f251e8b5f022fdbf4ed8d479309
Aisha Khan <aishasuhail2005@gmail.com>
2025-09-24 17:56:57 -0600

    added color to shape

--- app/src/main/java/com/example/impact/Shape.java
+++ app/src/main/java/com/example/impact/Shape.java
@@ -3,7 +3,8 @@ package com.example.impact;
 public abstract class Shape {
     private Integer x;
     private Integer y;
-
+    private String color = "purple"; 
+    
     public Shape(Integer x, Integer y) {
         this.x = x;
         this.y = y;

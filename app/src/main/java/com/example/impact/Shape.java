99e39142cb863c87aaa269199b273d11d4df6762
Orion-W <oawarawa@gmail.com>
2025-09-24 17:48:33 -0600

    colours

--- app/src/main/java/com/example/impact/Shape.java
+++ app/src/main/java/com/example/impact/Shape.java
@@ -3,7 +3,7 @@ package com.example.impact;
 public abstract class Shape {
     private Integer x;
     private Integer y;
-
+    private String colour = "colours are dumb";
     public Shape(Integer x, Integer y) {
         this.x = x;
         this.y = y;

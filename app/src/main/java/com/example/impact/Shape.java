6fb1f23e7aad1dea64c1c1543eb042f84076ab45
MisbahAN <misbahahmed2005@gmail.com>
2025-09-24 18:10:29 -0600

    Added color magenta.

--- app/src/main/java/com/example/impact/Shape.java
+++ app/src/main/java/com/example/impact/Shape.java
@@ -4,6 +4,8 @@ public abstract class Shape {
     private Integer x;
     private Integer y;
 
+    private String color = "magenta";
+
     public Shape(Integer x, Integer y) {
         this.x = x;
         this.y = y;

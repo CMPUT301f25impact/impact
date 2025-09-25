179faa2194c82494d788d553f3b07c58be243faf
ansh10 <anshkoladia@quotograph.io>
2025-09-24 18:09:20 -0600

    color

--- app/src/main/java/com/example/impact/Shape.java
+++ app/src/main/java/com/example/impact/Shape.java
@@ -3,11 +3,12 @@ package com.example.impact;
 public abstract class Shape {
     private Integer x;
     private Integer y;
-
+    private String color = "blue";
     public Shape(Integer x, Integer y) {
         this.x = x;
         this.y = y;
     }
+
 
     public Integer getX() {
         return x;
@@ -17,4 +18,11 @@ public abstract class Shape {
         return y;
     }
 
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
 }
